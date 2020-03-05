cb10302bee958577e7e63122876fabe1bc8d2c56
AymanMaalej <Maalej.Ayman98@gmail.com>
2020-03-05 11:03:55 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TesttingDirective } from './testting.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TesttingDirective
   ],
   imports: [
     BrowserModule,
