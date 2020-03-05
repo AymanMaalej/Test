164716c9ae810d00f81588600c8b834ce0ceaee1
AymanMaalej <Maalej.Ayman98@gmail.com>
2020-03-05 12:30:55 +0100

    Binding Done

--- src/app/binding/binding.component.ts
+++ src/app/binding/binding.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgModule } from '@angular/core';
 @Component({
   selector: 'app-binding',
   templateUrl: './binding.component.html',
   styleUrls: ['./binding.component.scss']
 })
 export class BindingComponent implements OnInit {
-
+  value = '';
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onClick() {
+    this.value = '';
+  }
 }
